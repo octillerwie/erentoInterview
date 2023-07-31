3d60e2c4e6dd69094e5082f16c48d6400a610844
Frank van Luling <octillerwie@gmail.com>
2023-07-31 22:18:30 +0200

    get all cities endpoint, tested in postman, works

--- interview-backend/src/app.controller.ts
+++ interview-backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cities } from './app.service';
 
-@Controller()
+@Controller('api/cities')
+
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getCities(): Cities[] {
+    try {
+      return this.appService.getCities();
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
--- interview-backend/src/app.service.ts
+++ interview-backend/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
 
+export interface Cities {
+  uuid: string;
+  cityName: string;
+  count: number;
+}
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private cities: Array<Cities>;
+
+  constructor() {
+    const filePath = path.resolve(__dirname, '..', '..', 'cities.json');
+    this.cities = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  }
+
+  getCities(): Cities[] {
+    return this.cities;
   }
 }
