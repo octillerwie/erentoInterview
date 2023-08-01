890076abdc5b0ff6c154f52ccbc227ba99bdec19
Frank van Luling <octillerwie@gmail.com>
2023-08-01 18:22:49 +0200

    added the search form and display to FE

--- interview-frontend/src/app/app.component.ts
+++ interview-frontend/src/app/app.component.ts
@@ -1,30 +1,52 @@
 import { Component } from '@angular/core';
 import { CitiesService } from './cities.service';
+
 interface City {
   uuid: string;
   cityName: string;
   count: number;
 }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   cities: City[];
+  searchResults: City[];
   city: string;
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  
   constructor(
     private citiesService: CitiesService,
   ) {
     this.cities = [];
+    this.searchResults = [];
     this.city = '';
   }
   title = 'city-ui';
+
   ngOnInit() {
     this.citiesService.getCities().subscribe((data) => {
       console.log(data);
       this.cities = data as City[];
     });
   }
+
+  onSearch() {
+    // no input = no result
+    if (this.city.trim() === '') {
+      this.searchResults = [];
+      return;
+    }
+
+    // filter cities with the input
+    this.searchResults = this.cities.filter((city) =>
+    city.cityName.toLowerCase().includes(this.city.toLowerCase())
+    );
+
+    // max 5 results
+    this.searchResults = this.searchResults.slice(0, 5);
+  } 
 }
--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
